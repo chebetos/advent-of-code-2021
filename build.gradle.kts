98452139c76965910118cb0103c30c863ed895a0
César Augusto Delgado Rodríguez <chebetos@gmail.com>
2021-12-06 19:06:57 +0100

    Updating template scaffold to a standard Gradle Application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.0"
+    application
 }
+
+group = "es.chebetos"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
 tasks {
-    sourceSets {
-        main {
-            java.srcDirs("src")
-        }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    test {
+        useJUnitPlatform()
     }
 
     wrapper {
         gradleVersion = "7.3"
     }
+}
+
+application {
+    mainClass.set("MainKt")
 }
